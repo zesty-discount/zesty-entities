57ff76af495751484a86bf33b1d185b7e5bea62d
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-03-31 19:51:37 +0300

    Update min ios version to 17

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "id5-entities",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "Entities", targets: ["Entities"]),

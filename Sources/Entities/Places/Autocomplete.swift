cebe25820973adb57318f506a9ab5f59c4281659
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-04-17 17:10:30 +0300

    Add places autocomplete

---
+++ Sources/Entities/Places/Autocomplete.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public extension Places {
+    enum Autocomplete {
+        public struct Request: Codable, Equatable {
+            public let query: String
+            
+            public init(query: String) {
+                self.query = query
+            }
+        }
+        
+        public struct Response: Codable, Equatable {
+            public struct Suggestion: Codable, Equatable {
+                public let mainText: String
+                public let secondaryText: String
+                
+                public init(mainText: String, secondaryText: String) {
+                    self.mainText = mainText
+                    self.secondaryText = secondaryText
+                }
+            }
+            
+            public let suggestions: [Suggestion]
+            
+            public init(suggestions: [Suggestion]) {
+                self.suggestions = suggestions
+            }
+        }
+    }
+}

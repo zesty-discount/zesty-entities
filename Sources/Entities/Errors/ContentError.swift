d58f7e0c794e0a13c654cb42c0d4885e651d1acc
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-04-28 18:16:54 +0300

    Add new content errors

--- Sources/Entities/Errors/ContentError.swift
+++ Sources/Entities/Errors/ContentError.swift
@@ -2,7 +2,13 @@ import Foundation
 
 public enum ContentError: String, IdentifiableError {
     
-    case contentNotFound = "content_not_found"
+    case postNotFound = "post_not_found"
+    case parentCommentNotFound = "parent_comment_not_found"
+    case geocodingReturnedNoResults = "geocoding_no_results"
+    case mediaToDownloadNotFound = "media_not_found"
+    case challengeTokenNotFound = "challenge_token_not_found"
+    case postContainsNoImages = "post_contains_no_images"
+    case cannotFetchMedia = "cannot_fetch_media"
     
     public var identifier: String {
         rawValue
@@ -10,8 +16,20 @@ public enum ContentError: String, IdentifiableError {
     
     public var reason: String {
         switch self {
-        case .contentNotFound:
-            return "Requested content not found"
+        case .postNotFound:
+            "Post not found"
+        case .parentCommentNotFound:
+            "Parent comment not found"
+        case .geocodingReturnedNoResults:
+            "Geocoding returned no results"
+        case .mediaToDownloadNotFound:
+            "Media to download not found"
+        case .challengeTokenNotFound:
+            "Challenge token not found"
+        case .postContainsNoImages:
+            "Post contains no images"
+        case .cannotFetchMedia:
+            "Cannot fetch media from remote"
         }
     }
 }

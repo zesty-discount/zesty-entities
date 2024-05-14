f62628f64218ed6b1cd6335b15558caeb8fda336
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-05-14 09:46:39 +0300

    Add update device

--- Sources/Entities/Device/Device.swift
+++ Sources/Entities/Device/Device.swift
@@ -42,6 +42,43 @@ public enum Device {
         }
     }
     
+    public enum Update {
+        public struct Request: Codable, Equatable {
+            public let system: System?
+            public let osVersion: String?
+            public let pushToken: String?
+            
+            public init(
+                system: System?,
+                osVersion: String?,
+                pushToken: String?
+            ) {
+                self.system = system
+                self.osVersion = osVersion
+                self.pushToken = pushToken
+            }
+        }
+        
+        public struct Response: Codable, Equatable {
+            public let id: UUID
+            public let system: System
+            public let osVersion: String
+            public let pushToken: String?
+            
+            public init(
+                id: UUID,
+                system: System,
+                osVersion: String,
+                pushToken: String?
+            ) {
+                self.id = id
+                self.system = system
+                self.osVersion = osVersion
+                self.pushToken = pushToken
+            }
+        }
+    }
+
     public enum Detail {
         public struct Request: Codable, Equatable {}
         

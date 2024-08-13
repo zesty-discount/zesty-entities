c687256f67ddbf9f82d81d7cd2c5b20dcfafe2a3
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-08-13 19:44:07 +0300

    Change name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "id5-entities",
+    name: "zesty-entities",
     platforms: [.macOS(.v10_15), .iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
